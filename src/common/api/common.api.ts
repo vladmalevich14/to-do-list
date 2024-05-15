c6b19c39f538779b2465f4fd5b4e69853a8e96ac
vladmalevich14 <vladmalevich1431@gmail.com>
2024-05-15 20:56:05 +0300

    Added third-party cookies notification

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,7 +4,6 @@ export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
   withCredentials: true,
   headers: {
-    // "API-KEY": "1cdd9f77-c60e-4af5-b194-659e4ebd5d41",
     "API-KEY": "d53fee38-d720-4858-a5f9-38a6410b5751",
   }
 })
